dc2d5ffee195b276d948abba24c7078be6cd2cdd
OZDimensional <2433361732@qq.com>
2024-05-13 17:41:59 +0800

    test for branch

---
+++ app/judgement/service/judging/cpp_judgement.go
@@ -0,0 +1,7 @@
+package judging
+
+import pb "online-judge/proto"
+
+func JudgeCpp(request *pb.SubmitRequest) (int, error) {
+
+}
---
+++ pkg/resp/language-kind.go
@@ -0,0 +1,8 @@
+package resp
+
+const (
+	CPP = 2000 + iota
+	JAVA
+	PYTHON
+	GO
+)
