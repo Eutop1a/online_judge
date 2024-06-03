2ff55b4ddcac92d177fc8ef921197369661b29f2
OZDimensional <2433361732@qq.com>
2024-06-03 18:44:20 +0800

    changing

--- app/judgement/service/judging/python/python_judgement.go
+++ app/judgement/service/judging/python/python_judgement.go
@@ -81,7 +81,8 @@ func JudgePYTHON(request *pb.SubmitRequest, response *pb.SubmitResponse) (*pb.Su
 			//TODO:处理答案错误
 
 			if expected[i] != string(output) {
-				fmt.Println("test Output: ", expected[i])
+				response.Output = fmt.Sprintf("Intput: %s\nExpected: %s\nOutput: %s", input[i], expected[i], string(output))
+				fmt.Println(response.Output)
 				WA <- 1
 				return
 			}
